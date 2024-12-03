0235da90f632262a9d29a6296ebafcba67893ad6
subina <subina.m@PITAD.local>
2024-12-03 17:18:44 +0545

    CRUD using dapper

---
+++ CrudDapper/CrudDapper/Interfaces/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using CrudDapper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudDapper.Interfaces
+{
+    public interface IProductService
+    {
+        public Task<IEnumerable<Product>> GetProductsAsync();
+        public Task<Product> GetProductByIdAsync(int id);
+        public Task<int> AddProductAsync(Product product);
+        public Task UpdateProductAsync(int id, Product product);
+        public Task DeleteProductAsync(int id);
+
+    }
+}
---
+++ CrudDapper/CrudDapper/Services/ProductService.cs
@@ -0,0 +1,59 @@
+﻿using CrudDapper.Interfaces;
+using CrudDapper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudDapper.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _repository;
+        public ProductService(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _repository.GetProductsAsync();
+        }
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _repository.GetProductByIdAsync(id);
+        }
+        public async Task<int> AddProductAsync(Product product)
+        {
+            if(product.Price <= 0)
+            {
+                throw new ArgumentException("price must be greater than zero");
+            }
+            return await _repository.AddProductAsync(product);
+        }
+        public async Task UpdateProductAsync(int id,Product product)
+        {
+            var existingProduct = await _repository.GetProductByIdAsync(id);
+            if (existingProduct == null)
+            {
+                throw new KeyNotFoundException("Product not found");
+            }
+            if(product.Price <= 0)
+            {
+                throw new ArgumentException("price must be greater than zero");
+            }
+            await _repository.UpdateProductAsync(id,product);
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            var product = await _repository.GetProductByIdAsync(id);
+            if (product == null)
+            {
+                throw new KeyNotFoundException("product not found.");
+            }
+            await _repository.DeleteProductAsync(id);
+        }
+
+    }
+}
